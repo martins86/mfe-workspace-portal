fb22e1dcb214c895d21e440919a5ef13e2894cb1
Thiago Martins de Freitas <thiagodf@ciandt.com>
2021-11-12 18:53:35 -0300

    Ajustes no tabWidth do prettier

--- projects/portal/src/app/app.component.ts
+++ projects/portal/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core'
 
 @Component({
-    selector: 'mfe-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+  selector: 'mfe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    title = 'portal'
+  title = 'portal'
 }
