0e0409189f4b6c36cf1175a75e00371d0da270a9
martins86 <martins86@gmail.com>
2021-11-13 21:52:43 +0000

    Iniciando o Footer

--- projects/portal/src/app/app.component.ts
+++ projects/portal/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'portal'
+  title = 'portal';
+  currentYear = new Date().getFullYear();
 }
