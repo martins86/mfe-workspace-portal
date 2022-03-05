c901bf3a3b9e638e76545385d9fa3528e8d80148
Thiago Martins de Freitas <martins86@gmail.com>
2022-03-04 21:26:54 -0300

    test(layout): corrigindo imports

--- projects/portal/src/app/layout/default/default.component.spec.ts
+++ projects/portal/src/app/layout/default/default.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { DefaultComponent } from './default.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 describe('DefaultComponent', () => {
   let component: DefaultComponent;
@@ -10,7 +12,7 @@ describe('DefaultComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [DefaultComponent],
+      declarations: [DefaultComponent, HeaderComponent, FooterComponent],
     }).compileComponents();
   });
 
